d5909d418f2b0e10177ced85880f09e54aeee5bd
Mojtaba Michael <mojimich2015@gmail.com>
2026-03-06 18:53:24 +0300

    Add auth jwt config

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module github.com/mjmichael73/go-uber-clone
 
 go 1.25.5
+
+require (
+	github.com/gin-gonic/gin v1.9.1
+	github.com/golang-jwt/jwt/v5 v5.0.0
+	github.com/google/uuid v1.3.1
+	github.com/gorilla/websocket v1.5.0
+	github.com/lib/pq v1.10.9
+	github.com/nats-io/nats.go v1.31.0
+	github.com/redis/go-redis/v9 v9.2.1
+	golang.org/x/crypto v0.14.0
+	google.golang.org/grpc v1.59.0
+	google.golang.org/protobuf v1.31.0
+)
---
+++ pkg/auth/jwt.go
@@ -0,0 +1,67 @@
+package auth
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var (
+	ErrInvalidToken = errors.New("invalid token")
+	ErrExpiredToken = errors.New("token has expired")
+)
+
+type Claims struct {
+	UserID   string `json:"user_id"`
+	UserType string `json:"user_type"`
+	Email    string `json:"email"`
+	jwt.RegisteredClaims
+}
+
+type JWTManager struct {
+	secretKey     string
+	tokenDuration time.Duration
+}
+
+func NewJWTManager(secretKey string, tokenDuration time.Duration) *JWTManager {
+	return &JWTManager{
+		secretKey:     secretKey,
+		tokenDuration: tokenDuration,
+	}
+}
+
+func (m *JWTManager) Generate(userID, userType, email string) (string, error) {
+	claims := &Claims{
+		UserID:   userID,
+		UserType: userType,
+		Email:    email,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(m.tokenDuration)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			Subject:   userID,
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(m.secretKey))
+}
+
+func (m *JWTManager) Verify(tokenStr string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenStr, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, ErrInvalidToken
+		}
+		return []byte(m.secretKey), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(*Claims)
+	if !ok || !token.Valid {
+		return nil, ErrInvalidToken
+	}
+
+	return claims, nil
+}
